aa2cb6ff3a14d5a0291788385bf93a5f9a7dcafd
scmo-ops <78674164+scmo-ops@users.noreply.github.com>
2021-07-09 15:48:54 -0500

    First step

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,1 +1,20 @@
 // @TODO: YOUR CODE HERE!
+
+// Def the margins with the window size
+
+var svgWidth = 900;
+var svgHeight = 360;
+
+// step 2: define margins
+var margin = {
+  top: 50,
+  right: 50,
+  bottom: 50,
+  left: 50
+};
+
+var height = svgHeight - margin.top - margin.bottom;
+var width = svgWidth - margin.left - margin.right;
+
+// step 3: importing the data
+

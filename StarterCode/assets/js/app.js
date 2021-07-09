151ddda512227161971edb39c21b58543a6525b5
scmo-ops <78674164+scmo-ops@users.noreply.github.com>
2021-07-09 15:51:50 -0500

    scales

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -17,4 +17,23 @@ var height = svgHeight - margin.top - margin.bottom;
 var width = svgWidth - margin.left - margin.right;
 
 // step 3: importing the data
+var pizzasEatenByMonth = [15, 5, 25, 18, 12, 22, 0, 4, 15, 10, 21, 2];
+
+// step 4: append svg and group 
+var svg = d3.select("#scatter")
+  .append("svg")
+  .attr("height", svgHeight)
+  .attr("width", svgWidth);
+
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// step 5: set up the scales
+var xScale = d3.scaleLinear()
+  .domain([0, pizzasEatenByMonth.length])
+  .range([0, width]);
+
+var yScale = d3.scaleLinear()
+  .domain([0, d3.max(pizzasEatenByMonth)])
+  .range([height, 0]);
 
